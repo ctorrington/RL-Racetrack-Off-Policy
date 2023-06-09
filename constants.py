2bf6907868dac3d12c16ce05d48e8bc6ce1acf40
ctorrington1 <ctorrington1@gmail.com>
2023-06-09 13:27:50 +0100

    actions constants

--- constants.py
+++ constants.py
@@ -7,3 +7,18 @@ class Constants:
         @staticmethod
         def as_tuple():
             return Constants.SURFACES.TRACK, Constants.SURFACES.GRAVEL
+        
+    class ACTIONS:
+        X_UP_Y_UP = "X_UP_Y_UP"
+        X_UP_Y_DOWN = "X_UP_Y_DOWN"
+        X_UP_Y_STAY = "X_UP_Y_STAY"
+        X_DOWN_Y_UP = "X_DOWN_Y_UP"
+        X_DOWN_Y_DOWN = "X_DOWN_Y_DOWN"
+        X_DOWN_Y_STAY = "X_DOWN_Y_STAY"
+        X_STAY_Y_UP = "X_STAY_Y_UP"
+        X_STAY_Y_DOWN = "X_STAY_Y_DOWN"
+        X_STAY_Y_STAY = "X_STAY_Y_STAY"
+        
+        @staticmethod
+        def as_tuple():
+            return Constants.ACTIONS.X_UP_Y_UP, Constants.ACTIONS.X_UP_Y_DOWN, Constants.ACTIONS.X_UP_Y_STAY, Constants.ACTIONS.X_DOWN_Y_UP, Constants.ACTIONS.X_DOWN_Y_DOWN, Constants.ACTIONS.X_DOWN_Y_STAY, Constants.ACTIONS.X_STAY_Y_UP, Constants.ACTIONS.X_STAY_Y_DOWN, Constants.ACTIONS.X_STAY_Y_STAY
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Dependancies.
 import numpy as np
 import matplotlib.pyplot as plt
+# import noise
 
 # Libraries
 import random
@@ -14,32 +15,72 @@ class Racetrack:
         """Initialise the environment."""
         
         self.SURFACES = Constants.SURFACES
+        self.ACTIONS = Constants.ACTIONS
         
         # Setting the dimensions for the environment.
-        self.x_range = range(0, 10)
-        self.y_range = range(0, 10)
+        self.x_range = range(0, 20)
+        self.y_range = range(0, 20)
         self.state_space = {}
+        self.maximum_velocity = 5
         
+        state_counter = 0
         for x in self.x_range:
             for y in self.y_range:
-                state = (x, y, random.choice(self.SURFACES.as_tuple()))
+                for v_x in range(self.maximum_velocity):
+                    for v_y in range(self.maximum_velocity):
+                        state = (x, y, (v_x, v_y), random.choice(self.SURFACES.as_tuple()))
+                        
+                        # State values.
+                        self.state_space[state] = {
+                            "actions": {},
+                            "policies": {},
+                            "estimated return": 0,
+                            "state entries": 0,
+                        }
+                        
+                        # State actions.
+                        for action in self.ACTIONS.as_tuple():
+                            self.state_space[state]["actions"][action] = {
+                                "value": 0,
+                                "cumulative weight": 0,
+                            }
+                            
+                        state_counter += 1
+                        
+        print(f"Generated a field of size {len(self.x_range)} by {len(self.y_range)}.")
+        print(f"Created {state_counter} states.")
+    
+    def generate_race_track(self):
+        """Generate the race track."""
+        
+        scale = 100.0
+        octaves = 6
+        persistence = 0.5
+        lacunarity = 2.0
+        
+        track = np.zeros((self.x_range, self.y_range))
+        
+        for x in range(self.x_range):
+            for y in range(self.y_range):
+                track[x][y] = noise.pnoise2(x / scale,
+                                            y / scale,
+                                            octaves=octaves,
+                                            persistence=persistence,
+                                            lacunarity=lacunarity,
+                                            repeatx=self.x_range,
+                                            repeaty=self.y_range,
+                                            base=0)
                 
-                # State values.
-                self.state_space[state] = {
-                    "state actions": {},
-                    "policies": {},
-                    "estimated return": 0,
-                    "state entries": 0,
-                }
-                
-                # State actions.
+        plt.show(track, cmap="viridis")
+        plt.show()
+        
     
     def plot_track(self):
         """Plot the track."""
         
         surfaces = np.zeros((len(self.x_range), len(self.y_range)))
         for state in self.state_space:
-            x, y, surface = state
+            x, y, (vx, vy), surface = state
             if surface == self.SURFACES.TRACK:
                 surface = 1
             else:
